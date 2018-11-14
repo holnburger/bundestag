d66a894ce42d2d41c0d7549b22f7121c6cf702dd
Josef Holnburger <josef@holnburger.com>
2018-11-14 16:49:12 +0100

    chapter 3 added

--- functions/get_overview_df.R
+++ functions/get_overview_df.R
@@ -5,6 +5,11 @@ get_overview_df <- function(x){
   redner_nachname <- x %>% xml_node("redner") %>% xml_node("nachname") %>% xml_text()
   redner_fraktion <- x %>% xml_node("redner") %>% xml_node("fraktion") %>% xml_text()
   redner_rolle <- x %>% xml_node("rolle_kurz") %>% xml_text()
+  sitzung <- x %>% xml_find_first("//sitzungsnr") %>% xml_text() %>% as.integer()
+  datum <- x %>% xml_find_first("//datum") %>% xml_attr("date") %>% lubridate::dmy()
+  wahlperiode <- x %>% xml_find_first("//wahlperiode") %>% xml_text() %>% as.integer()
   
-  data_frame(rede_id, redner_id, redner_vorname, redner_nachname, redner_fraktion, redner_rolle)
+  
+  
+  data_frame(rede_id, redner_id, redner_vorname, redner_nachname, redner_fraktion, redner_rolle, sitzung, datum, wahlperiode)
 }
